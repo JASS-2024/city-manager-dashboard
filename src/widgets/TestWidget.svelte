2e3e6469d4428cd3304483f74df40299e912c923
Hamish Starling <hamishstarling@hotmail.co.uk>
2024-04-01 19:27:43 +0300

    Update test widget for duck

--- src/widgets/TestWidget.svelte
+++ src/widgets/TestWidget.svelte
@@ -1,3 +1,20 @@
-<div>
-    <p>Hello</p>
+<div id="testWidget">
+    <div class="spacer"></div>
+    <p id="duck">🦆</p>
+    <div class="spacer"></div>
 </div>
+
+<style>
+    #testWidget {
+        width: 100%;
+        height: 100%;
+        background-color: var(--color);
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+    }
+    #duck {
+        font-size: 30pt;
+        margin: 0;
+    }
+</style>
