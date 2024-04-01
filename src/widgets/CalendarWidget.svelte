6ecaf7c0db7761f5f38ce754022cde8449289748
Hamish Starling <hamishstarling@hotmail.co.uk>
2024-04-01 22:46:30 +0300

    Use eventContent to customise text

--- src/widgets/CalendarWidget.svelte
+++ src/widgets/CalendarWidget.svelte
@@ -13,7 +13,8 @@
             {id: "Event 2", start: new Date("2024-04-02T18:00:00.000"), end: new Date("2024-04-02T19:00:00.000")},
             {id: "Event 3", start: new Date("2024-04-03T18:00:00.000"), end: new Date("2024-04-03T19:00:00.000")},
             {id: "Event 4", start: new Date("2024-04-04T18:00:00.000"), end: new Date("2024-04-04T19:00:00.000")},
-        ]
+        ],
+        eventContent: () => "small"
     };
 </script>
 
