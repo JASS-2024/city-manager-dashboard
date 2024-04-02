c7287243b42be36f26ea27afded31ff81df5604e
Julian Kraus <>
2024-04-02 10:44:56 +0300

    add map for grid

--- src/widgets/shared/Map.svelte
+++ src/widgets/shared/Map.svelte
@@ -1,27 +1,72 @@
-<script lang="ts"> 
-enum Tile {
-  RoadVertical,
-  RoadHorizontal,
-  Blank
-}
-let map = [[Tile.RoadHorizontal, Tile.RoadHorizontal]]
+<script lang="ts">
+  import TestWidget from "../TestWidget.svelte";
 
-function visualize(tile: Tile) {
-  switch tile {
-    case Tile.RoadVertical: "="
-    case Tile.RoadHorizontal: "|"
-    case Tile.Blank: " "
+  enum Tile {
+    RoadVertical,
+    RoadHorizontal,
+    Blank,
+    CurveBL,
+    CurveBR,
+    CurveTR,
+    CurveTL,
+    GarageBottom,
   }
-}
+
+  let map = [
+    [
+      Tile.CurveBR,
+      Tile.RoadHorizontal,
+      Tile.RoadHorizontal,
+      Tile.RoadHorizontal,
+      Tile.Blank,
+      Tile.Blank,
+      Tile.Blank,
+    ],
+    [
+      Tile.RoadHorizontal,
+      Tile.GarageBottom,
+      Tile.GarageBottom,
+      Tile.GarageBottom,
+      Tile.Blank,
+      Tile.Blank,
+      Tile.Blank,
+    ],
+    [
+      Tile.CurveTR,
+      Tile.RoadHorizontal,
+      Tile.RoadHorizontal,
+      Tile.RoadHorizontal,
+      Tile.CurveTL,
+    ],
+  ];
+
+  function visualize(tile: Tile) {
+    switch (tile) {
+      case Tile.RoadVertical:
+        return "|";
+      case Tile.RoadHorizontal:
+        return "=";
+      case Tile.Blank:
+        return " ";
+      case Tile.CurveBL:
+        return "T";
+      case Tile.CurveBR:
+        return "T";
+      case Tile.CurveTR:
+        return "L";
+      case Tile.CurveTL:
+        return "2";
+      case Tile.GarageBottom:
+        return "^";
+    }
+  }
 </script>
 
 <div>
   {#each map as row}
     {#each row as tile}
-      {
-        visualize(tile)
-      }  
+      {visualize(tile)}
     {/each}
-  "\n"
+    <br />
   {/each}
 </div>
