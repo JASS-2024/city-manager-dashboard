cb67dc7e176999a8c170780960eeb37e7bf89dbe
Hamish Starling <hamishstarling@hotmail.co.uk>
2024-04-04 17:40:28 +0300

    Implement prototype for retrieving LED data

---
+++ src/widgets/shared/BlackLed.svelte
@@ -0,0 +1,27 @@
+
+<div class="circle-container">
+    <div class="circle-black"></div>
+</div>
+
+<style>
+.circle-container {
+    width: 10px;
+    height: 10px;
+    
+    vertical-align: middle;
+    text-align: center;
+     display: inline-block;
+     align-items: center;
+    justify-content: center;
+}
+.circle-black {
+    width: 6px;
+    height: 6px;
+    display: inline-block;
+    border-radius: 50%;
+    background-color: black;
+    box-shadow: 0 0 0px 1px white,
+                0 0 8px 4px gray,
+    /* box-shadow: 0 0 3px 2px var(--colour), 0 0 4px 2px rgb(234, 198, 255), 0 0 6px 4px rgb(211, 203, 215); */
+}
+</style>
---
+++ src/widgets/shared/Led.svelte
@@ -0,0 +1,31 @@
+
+<div class="circle-container">
+    <div class="circle"></div>
+</div>
+
+<style>
+.circle-container {
+    width: 10px;
+    height: 10px;
+    vertical-align: middle;
+    text-align: center;
+    justify-content: center;
+    display: inline-block;
+}
+
+.circle {
+    width: 1px;
+    height: 1px;
+    display: inline-block;
+    margin: 8px;
+    border-radius: 50%;
+    background-color: white;
+    box-shadow: 0 0 2px 1px white,
+                0 0 3px 1.5px white,
+                0 0 4px 2px white,
+                0 0 5px 2px oklch(from var(--colour) calc(l * 1.4) 0.6 h),
+                0 0 6px 2px oklch(from var(--colour) calc(l * 1.2) 0.6 h), 
+                0 0 8px 4px oklch(from var(--colour) calc(l * 1.3) 0.6 h);
+    /* box-shadow: 0 0 3px 2px var(--colour), 0 0 4px 2px rgb(234, 198, 255), 0 0 6px 4px rgb(211, 203, 215); */
+}
+</style>
