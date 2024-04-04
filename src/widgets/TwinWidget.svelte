03c8815ac46185574ecc33662047aa78fab18c8c
Hamish Starling <hamishstarling@hotmail.co.uk>
2024-04-04 17:07:31 +0300

    Separate out MQTT

--- src/widgets/TwinWidget.svelte
+++ src/widgets/TwinWidget.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
-    import mqtt from "mqtt";
     import { onMount } from "svelte";
     
     import Map from "./shared/Map.svelte";
+  import { subscribe } from "$lib/mqtt";
+  import type { OnMessageCallback } from "mqtt";
     
     type Positions = {
         [key: string]: [number, number];
     };
-    
-    
     
     let tileSize = 10;
     
@@ -31,42 +30,25 @@
         return (x / 10) * tile
     }
     
+    const updateDuckiePosition: OnMessageCallback = (topic, message) => {
+        const jsonObject = JSON.parse(message.toString());
+
+        // Safely access the x and y values
+        const x = jsonObject?.data?.coordinates?.x;
+        const y = jsonObject?.data?.coordinates?.y;
+
+        if (typeof x === "number" && typeof y === "number") {
+            // Store the coordinates as an array [x, y] against the topic key
+            positions = {...positions, [topic]: [x,y]}
+            console.log(`x: ${x} y: ${y}`)
+        } else {
+            console.log(`Invalid coordinates received for topic ${topic}`);
+        }
+    }
+
     onMount(() => {
-        const URL = "ws://192.168.3.85:9001" // wss://test.mosquitto.org:8081
-        const TOPIC = "vehicle/+/status"
-        const TOPIC2 = "camera/+/status"
-    
-        const client = mqtt.connect(URL);
-    
-        client.on("connect", () => {
-            client.subscribe(TOPIC2, (err) => {
-                if (err) {
-                    console.log(err)
-                }
-            });
-            client.subscribe(TOPIC, (err) => {
-                if (err) {
-                    console.log(err)
-                }
-            });
-        });
-    
-        client.on("message", (topic, message) => {
-            console.log(`message: ${topic}`)
-            const jsonObject = JSON.parse(message.toString());
-    
-            // Safely access the x and y values
-            const x = jsonObject?.data?.coordinates?.x;
-            const y = jsonObject?.data?.coordinates?.y;
-    
-            if (typeof x === "number" && typeof y === "number") {
-                // Store the coordinates as an array [x, y] against the topic key
-                positions = {...positions, [topic]: [x,y]}
-                console.log(`x: ${x} y: ${y}`)
-            } else {
-                console.log(`Invalid coordinates received for topic ${topic}`);
-            }
-        });
+        subscribe("vehicle/+/status", updateDuckiePosition)
+        subscribe("camera/+/status", updateDuckiePosition)
     })
     </script>
     
